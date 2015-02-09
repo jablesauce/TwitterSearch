30aef1468a7c0e19033e6e19c56b061149670491
James <jamesbonello9@gmail.com>
2015-02-09 19:34:19 +0100

    Added config.php and change callback URL

---
+++ twitteroauth-master/config.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @file
+ * A single location to store configuration.
+ */
+
+define('CONSUMER_KEY', 'JWb4h4vDppIJf6LtAgToqLPcR');
+define('CONSUMER_SECRET', 'akEV48TwWrz9UNXQKaqXAvkGgemyE7UbklXiYqduZRlj0KxHFV');
+define('ACCESS_TOKEN', '2886942669-DW8FDOiDQi9hgJbDK8G0eZjWMwTxiJuggJCCONc');
+define('ACCESS_TOKEN_SECRET', 'bIRnAof5ntZDXSJ90wVWmJXyLESNzncBLo5QAmblCfMQP');
+define('OAUTH_CALLBACK', 'http://twittersearch-jbon0041.rhcloud.com/TwitterSearch/twitteroauth-master/callback.php');
+
+define('INDEX_PATH', 'index.json');
+define('CLASSIFY_KEY', '3qkum0UtyYnQqfhPG5cK6t8zYM');
+?>
