be40f299913be3b826cdc5e894e24d2a99821e06
James <jamesbonello9@gmail.com>
2015-02-17 23:19:34 +0100

    ececc

--- search.php
+++ search.php
@@ -49,7 +49,7 @@ if (! empty ( $intersection )) {
 	$time = 'Total search time: ' . ($t_end - $t_start)/60;	// total searching time
 	$flag = 1;
 } else {
-	flag = 0;
+	$flag = 0;
 	$content =  "No relevant tweets found!";
 	$place = 'No location specified';
 	$t_end = microtime(true); // end searching
