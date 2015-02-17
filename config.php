b00ee32d3741a1169444e32782ef6fe814508884
James <jamesbonello9@gmail.com>
2015-02-17 23:03:41 +0100

    cedvevevd

--- config.php
+++ config.php
@@ -11,9 +11,9 @@ define('ACCESS_TOKEN', '2886942669-DW8FDOiDQi9hgJbDK8G0eZjWMwTxiJuggJCCONc');
 define('ACCESS_TOKEN_SECRET', 'bIRnAof5ntZDXSJ90wVWmJXyLESNzncBLo5QAmblCfMQP');
 //define('OAUTH_CALLBACK', 'http://twittersearch-jbon0041.rhcloud.com/app-root/runtime/repo/twitteroauth-master/callback.php');
 //define('OAUTH_CALLBACK', 'http://twittersearch-jbon0041.rhcloud.com/var/lib/openshift/54d8efc04382ecb1c900011c/app-root/runtime/repo/twitteroauth-master/callback.php');
-define('OAUTH_CALLBACK', 'http://localhost/TwitterSearch/callback.php');
+//define('OAUTH_CALLBACK', 'http://localhost/TwitterSearch/callback.php');
 //define('OAUTH_CALLBACK', 'http://www.uomtwittersearch.net/callback.php');
-//define('OAUTH_CALLBACK', 'http://uomtwittersearch-jbon0041.rhcloud.com/callback.php');
+define('OAUTH_CALLBACK', 'http://uomtwittersearch-jbon0041.rhcloud.com/callback.php');
 
 define('INDEX_PATH', 'index.json');
 define('CLASSIFY_KEY', '3qkum0UtyYnQqfhPG5cK6t8zYM');
